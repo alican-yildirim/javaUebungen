525023fd047623e44b8d62e554d2ca652fc8d9cf
alican yildirim <aliicann.yildirim@hotmail.com>
2022-08-18 15:26:40 +0200

    pw check version mit GUI (JFrame vom Packet swing) from Macbook

--- uebung_pwCheck_180822/src/App.java
+++ uebung_pwCheck_180822/src/App.java
@@ -1,6 +1,10 @@
 import java.util.Scanner;
 import java.util.HashMap;
 import javax.swing.*;
+import javax.swing.border.LineBorder;
+
+import java.awt.event.*;
+
 
 public class App {
 
@@ -9,46 +13,64 @@ public class App {
 
     public static HashMap<Integer,String> trysList = new HashMap<Integer,String>();
 
-    public static void scanInput() {
-            
-        Scanner scan = new Scanner(System.in);
-
-        if(scan.hasNextLine()) {
-
-            App.checkInput(scan.nextLine());
-
-        }
-
-    }
-
-    public static void checkInput(String input) {
+    public static void missErfolg(JLabel label,JTextField txt) {
         
-        if(App.pw.equals(input)){
-
-            System.out.println("Das Passwort war richtig!!!!\nSie haben " + wrongTrys + " Versuche gebraucht:");
-
-            for(Integer key : trysList.keySet()){
-
-                System.out.println(key + " - Versuch: " + trysList.get(key));
-
-            }
-
-        }else{
-
-            System.out.println("Das Passwort war NIIIIICHT richtig!!!!\nVersuchen Sie es nocheinmal!!!");
-            wrongTrys++;
-            trysList.put(wrongTrys, input);
-            App.scanInput();
-
-        }
+        label.setText("Das Passwort war NICHT richtig!!! Versuch es nocheinmal!");
+        wrongTrys++;
+        trysList.put(wrongTrys, txt.getText());
 
     }
 
     public static void main(String[] args) throws Exception {
 
-        System.out.println("Geben Sie Ihr Passwort ein!!!!");
+        JFrame frame = new JFrame("Passwort Überprüfer!!!!");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setBounds(300,300,600,400);
+        frame.setVisible(true);
+        JFrame.setDefaultLookAndFeelDecorated(true);
+        
 
-        App.scanInput();
+        JLabel label = new JLabel("Geben Sie Ihr Passwort ein!!!");
+        label.setVisible(true);
+        label.setBounds(600/3,50,600,50);
+        frame.add(label);
+
+        JTextField txtField = new JTextField();
+        txtField.setBounds(200,100,200,50);
+        frame.add(txtField);
+
+        JButton btn = new JButton("Prüfen!");
+        btn.setBounds(200,150,200,50);
+
+        btn.addActionListener(new ActionListener() {
+            
+            public void actionPerformed(ActionEvent e){
+                
+                if(App.pw.equals(txtField.getText())){
+
+                    String finallyOutput = "Das Passwort war richtig!!!!\nSie haben " + wrongTrys + " Versuche gebraucht:\n\n";
+
+                    for(Integer key : trysList.keySet()){
+                        
+                        finallyOutput += key + " - Versuch: " + trysList.get(key) + "\n";
+                        
+                    }
+
+                    label.setText(finallyOutput);
+                    txtField.setVisible(false);
+                    btn.setVisible(false);
+                    label.setBounds(0,0,6000,400);
+        
+                }else{
+
+                    App.missErfolg(label,txtField);
+
+                }        
+                
+            }
+            
+        });
+        frame.add(btn);
 
     }
 }
