70d453ed62b8ca509595f56177401f6bcf7e4c49
alican yildirim <aliicann.yildirim@hotmail.com>
2022-08-18 13:48:12 +0200

    pw check version ohne GUI from Macbook

--- uebung_pwCheck_180822/src/App.java
+++ uebung_pwCheck_180822/src/App.java
@@ -1,7 +1,54 @@
+import java.util.Scanner;
+import java.util.HashMap;
+import javax.swing.*;
+
 public class App {
-    public static void main(String[] args) throws Exception {
+
+    public static final String pw = "ali";
+    public static int wrongTrys = 0;
+
+    public static HashMap<Integer,String> trysList = new HashMap<Integer,String>();
+
+    public static void scanInput() {
+            
+        Scanner scan = new Scanner(System.in);
+
+        if(scan.hasNextLine()) {
+
+            App.checkInput(scan.nextLine());
+
+        }
+
+    }
+
+    public static void checkInput(String input) {
         
-        System.out.println("Hello, World!");
+        if(App.pw.equals(input)){
+
+            System.out.println("Das Passwort war richtig!!!!\nSie haben " + wrongTrys + " Versuche gebraucht:");
+
+            for(Integer key : trysList.keySet()){
+
+                System.out.println(key + " - Versuch: " + trysList.get(key));
+
+            }
+
+        }else{
+
+            System.out.println("Das Passwort war NIIIIICHT richtig!!!!\nVersuchen Sie es nocheinmal!!!");
+            wrongTrys++;
+            trysList.put(wrongTrys, input);
+            App.scanInput();
+
+        }
+
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        System.out.println("Geben Sie Ihr Passwort ein!!!!");
+
+        App.scanInput();
 
     }
 }
