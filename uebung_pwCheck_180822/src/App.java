700abeac372e624b4f4765dd135188b14aa86ff6
alican yildirim <aliicann.yildirim@hotmail.com>
2022-08-19 22:17:51 +0200

    pw-check update from Macbook

--- uebung_pwCheck_180822/src/App.java
+++ uebung_pwCheck_180822/src/App.java
@@ -1,21 +1,42 @@
+import java.lang.Thread;
 import java.util.HashMap;
 import javax.swing.*;
-
 import java.awt.event.*;
-
 
 public class App {
 
     private static final String pw = "ali";
-    public static int wrongTrys = 0;
+    private static int wrongTrys = 0;
 
     public static HashMap<Integer,String> trysList = new HashMap<Integer,String>();
 
-    public static void missErfolg(JLabel label,JTextField txt) {
+    private static void missErfolg(JLabel label,JTextField txt) {
         
         label.setText("Das Passwort war NICHT richtig!!! Versuch es nocheinmal!");
         wrongTrys++;
         trysList.put(wrongTrys, txt.getText());
+
+    }
+
+    private static void lowerProgressBar(JProgressBar progressBalken){
+
+        int subtractionOperand = progressBalken.getMaximum() / 5;
+
+        int newValue = progressBalken.getValue() - subtractionOperand;
+        //progressBalken.setValue(newValue);
+        for(int i = progressBalken.getValue();i > newValue;i--){
+
+            progressBalken.setValue(i);
+
+            try{
+
+                Thread.sleep(10);
+
+            }catch(InterruptedException e){
+                System.out.println("Fehler in der Methode lowerProgressBar!!!");
+            }
+            
+        }
 
     }
 
@@ -24,52 +45,70 @@ public class App {
         JFrame frame = new JFrame("Passwort Überprüfer!!!!");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setBounds(300,300,600,400);
-        frame.setVisible(true);
         JFrame.setDefaultLookAndFeelDecorated(true);
+        frame.setLayout(null);
         
-
         JLabel label = new JLabel("Geben Sie Ihr Passwort ein!!!");
         label.setVisible(true);
-        label.setBounds(600/3,50,600,50);
+        label.setBounds(600/3,20,600,50);
         frame.add(label);
 
         JTextField txtField = new JTextField();
-        txtField.setBounds(200,100,200,50);
+        txtField.setBounds(600/3,100,200,50);
         frame.add(txtField);
 
+        JProgressBar progressBalken = new JProgressBar(0,100);
+        progressBalken.setValue(100);
+        progressBalken.setBounds(200,50,200,50);
+        progressBalken.setVisible(true);
+        frame.add(progressBalken);
+
         JButton btn = new JButton("Prüfen!");
-        btn.setBounds(200,150,200,50);
+        btn.setBounds(600/3,150,200,50);
+
+        frame.add(btn);
+
+        frame.setVisible(true);
 
         btn.addActionListener(new ActionListener() {
             
             public void actionPerformed(ActionEvent e){
+
+                if(progressBalken.getValue() != 0){
+
+                    if(!txtField.getText().isEmpty()){
+
+                        if(App.pw.equals(txtField.getText())){
+
+                            String finallyOutput = "Das Passwort war richtig!!!!\nSie haben " + wrongTrys + " Versuche gebraucht:\n\n";
+
+                            for(Integer key : trysList.keySet()){
+                                
+                                finallyOutput += key + " - Versuch: " + trysList.get(key) + "\n";
+                                
+                            }
+
+                            label.setText(finallyOutput);
+                            txtField.setVisible(false);
+                            btn.setVisible(false);
+                            label.setBounds(0,0,6000,400);
                 
-                if(!txtField.getText().isEmpty()){
+                        }else{
 
-                    if(App.pw.equals(txtField.getText())){
+                            App.missErfolg(label,txtField);
+                            App.lowerProgressBar(progressBalken);
 
-                        String finallyOutput = "Das Passwort war richtig!!!!\nSie haben " + wrongTrys + " Versuche gebraucht:\n\n";
-
-                        for(Integer key : trysList.keySet()){
-                            
-                            finallyOutput += key + " - Versuch: " + trysList.get(key) + "\n";
-                            
                         }
 
-                        label.setText(finallyOutput);
-                        txtField.setVisible(false);
-                        btn.setVisible(false);
-                        label.setBounds(0,0,6000,400);
-            
                     }else{
 
-                        App.missErfolg(label,txtField);
+                        label.setText("Geben Sie etwas ein!!!");
 
                     }
 
                 }else{
 
-                    label.setText("Geben Sie etwas ein!!!");
+                    label.setText("Sie haben keine Chancen mehr!!!");
 
                 }
 
@@ -78,7 +117,7 @@ public class App {
             }
             
         });
-        frame.add(btn);
+
 
     }
 }
